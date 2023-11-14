b9130158b134a5cee629eabdff65cefda2c74d08
zahra sarabi <zahra.pouyagaran@gmail.com>
2023-11-14 16:01:47 +0330

    redux-example2

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const createStore = redux.createStore;
 
 // Define action type as a variable
 const CAKE_ORDERED = "CAKE_ORDERED";
-
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 // Action creator function
 const orderCake = () => ({ type: CAKE_ORDERED, quantity: 1 });
+const restockCake = (qty = 1) => ({ type: CAKE_RESTOCKED, quantity: qty });
 
 // Initial state
 const initialState = { numOfCake: 10 };
@@ -15,6 +16,8 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return { numOfCake: state.numOfCake - action.quantity };
+    case CAKE_RESTOCKED:
+      return { numOfCake: state.numOfCake + action.quantity };
     default:
       return state;
   }
@@ -33,6 +36,7 @@ const unsubscribe = store.subscribe(() => console.log("Updated State", store.get
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 // Unsubscribe from state changes
 unsubscribe();
