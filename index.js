695217bebbeb37a3bd5e3b468c4d6663cb1460fa
zahra sarabi <zahra.pouyagaran@gmail.com>
2023-11-15 09:49:50 +0330

    add Multiple Reducers

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ const bindActionCreators = redux.bindActionCreators;
 // Define action type as a variable
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+
 // Action creator function
 const orderCake = () => ({ type: CAKE_ORDERED, quantity: 1 });
 const restockCake = (qty = 1) => ({ type: CAKE_RESTOCKED, quantity: qty });
+const orderIceCream = (qty = 1) => ({ type: ICECREAM_ORDERED, quantity: qty });
+const restockIceCeram = (qty = 1) => ({ type: ICECREAM_RESTOCKED, quantity: qty });
 
 // Initial state
-const initialState = { numOfCake: 10 };
+const initialState = { numOfCake: 10, numOfIceCream: 20 };
 
 // Reducer function
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
-      return { numOfCake: state.numOfCake - action.quantity };
+      return { ...state, numOfCake: state.numOfCake - action.quantity };
     case CAKE_RESTOCKED:
-      return { numOfCake: state.numOfCake + action.quantity };
+      return { ...state, numOfCake: state.numOfCake + action.quantity };
+    case ICECREAM_ORDERED:
+      return { ...state, numOfIceCream: state.numOfIceCream - action.quantity };
+    case ICECREAM_RESTOCKED:
+      return { ...state, numOfIceCream: state.numOfIceCream + action.quantity };
     default:
       return state;
   }
@@ -39,10 +48,13 @@ store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(restockCake(3));
 //  instead of dispatch actions
-const action = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+const action = bindActionCreators({ orderCake, restockCake, restockIceCeram, orderIceCream }, store.dispatch);
 action.orderCake();
 action.orderCake();
 action.orderCake();
 action.restockCake(3);
+action.orderIceCream();
+action.orderIceCream();
+action.restockIceCeram(3);
 // Unsubscribe from state changes
 unsubscribe();
