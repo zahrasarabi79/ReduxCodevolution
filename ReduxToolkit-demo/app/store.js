aa7a657b6d7cdfdee41da96613a2c83cb45b8644
zahra sarabi <zahrasarabi1379@gmail.com>
2023-11-26 10:53:36 +0330

    add async Thunk

--- ReduxToolkit-demo/app/store.js
+++ ReduxToolkit-demo/app/store.js
@@ -2,10 +2,12 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice.js");
+
 //4-import icecreamSlice as icecreamReducer
 const icecreamReducer = require("../features/icecream/icecreamSlice.js");
 //6-use configure function that work like "combine Reducder function" in redux
+const userReducer = require("../features/user/userSlice.js");
 const logger = reduxLogger.createLogger();
-const store = configureStore({ reducer: { cake: cakeReducer, icecream: icecreamReducer }, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger) });
+const store = configureStore({ reducer: { cake: cakeReducer, icecream: icecreamReducer, user: userReducer }, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger) });
 //7-export store
 module.exports = store;
