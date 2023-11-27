114066c34fa4b08bcbebd6230fa26b462dcfcb97
zahra sarabi <zahrasarabi1379@gmail.com>
2023-11-27 09:54:55 +0330

    add config

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const CakeView = () => {
+  const numOfCakes = useSelector((state) => {
+    state.cake.numOfCakes;
+  });
   return (
     <div>
-      <h2>num of cake :</h2>
+      <h2>num of cake: {numOfCakes}</h2>
       <button>Order Cake</button>
       <button>Restock Cake</button>
     </div>
